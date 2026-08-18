581bb75171d3cf82d8e7afa439ed02db271da9eb
Orjionuchie Okechukwu David <okechukwuorjionuchie@gmail.com>
2026-08-18 14:52:15 +0100

    assignment II

---
+++ AssignmentII.py
@@ -0,0 +1,35 @@
+def two_sum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+
+
+# Example 1
+nums = [2, 7, 11, 15]
+target = 9
+print(two_sum(nums, target))
+# Output: [0, 1]
+
+
+
+
+
+
+# Example 2
+nums = [3, 2, 4]
+target = 6
+print(two_sum(nums, target))
+# Output: [1, 2]
+
+
+
+
+
+
+
+# Example 3
+nums = [3, 3]
+target = 6
+print(two_sum(nums, target))
+# Output: [0, 1]
